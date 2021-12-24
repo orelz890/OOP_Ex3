f9394deed0fbf93fbf367e137b57af85f33c932c
Orel Zamler <orelzx13@gmail.com>
2021-12-24 12:51:05 +0200

    Filled the shortest_path func not yet tested it

--- src/Implementation/GraphAlgo.py
+++ src/Implementation/GraphAlgo.py
@@ -52,21 +52,69 @@ class GraphAlgo(GraphAlgoInterface):
             return False
         return True
 
-    # def dijkstra(self, src: int):
-    #     node_q = queue.PriorityQueue()
-    #     for node in self.graph.nodes_dict.values():
-    #         if node.key == src:
-    #             node.w = 0.0
-    #         else:
-    #             node.w = float("inf")
-    #         node_q.put(node)
-    #     while node_q.not_empty:
-    #         node = node_q.get()
-    #
+    def dijkstra(self, src: int):
+        node_q = queue.PriorityQueue()
+        for node in self.graph.nodes_dict.values():
+            if node.key == src:
+                node.w = 0.0
+            else:
+                node.w = float("inf")
+            node_q.put(node)
 
+        while node_q.not_empty:
+            node = node_q.get()
+            for neighbour_edge in self.graph.out_edges.get(node.key).values():
+                neighbour_node = self.graph.nodes_dict.get(neighbour_edge.dest)
+                lowest_weight = node.w + neighbour_node.w
+                if lowest_weight < neighbour_node.w:
+                    neighbour_node.w = lowest_weight
+                    # להסתכל במימוש של גאווה
+
+    def set_all_w(self, w_val: float, tag_val: int):
+        for node in self.graph.nodes_dict.values():
+            node.w = w_val
+            node.tag = tag_val
 
     def shortest_path(self, id1: int, id2: int) -> (float, list):
-        pass
+        if self.graph.nodes_dict.get(id1) is None or self.graph.nodes_dict.get(id2) is None:
+            return float('inf'), []
+        src_node = self.graph.nodes_dict.get(id1)
+        dst_node = self.graph.nodes_dict.get(id2)
+        if id1 == id2:
+            return 0, [src_node]
+
+        self.set_all_w(float('inf'), -1)
+        node_q = queue.PriorityQueue()
+        node_q.put(src_node)
+        src_node.w = 0
+
+        while node_q.not_empty:
+            node = node_q.get()
+            for neighbour_edge in self.graph.out_edges.get(node.key).values():
+                neighbour_node = self.graph.nodes_dict.get(neighbour_edge.dest)
+                neighbours_new_weight = neighbour_edge.weight + node.w
+                if neighbour_node.w > neighbours_new_weight:
+                    neighbour_node.tag = node.key
+                    neighbour_node.w = neighbours_new_weight
+                    node_q.put(neighbour_node)
+
+        if dst_node.tag == -1:
+            return float('inf'), []
+        stack = [dst_node]
+        current_tag = dst_node.tag
+        total_dist = 0
+        while current_tag != id1:
+            current_node = self.graph.nodes_dict.get(current_tag)
+            total_dist += current_node.w
+            current_tag = current_node.tag
+            stack.append(current_node)
+        stack.append(src_node)
+
+        ans_list = []
+        while stack:
+            ans_list.append(stack.pop())
+        return total_dist, ans_list
+
 
     def TSP(self, node_lst: List[int]) -> (List[int], float):
         """
@@ -89,6 +137,7 @@ class GraphAlgo(GraphAlgoInterface):
         @return: None
         """
         raise NotImplementedError
+
 
 if __name__ == '__main__':
     g = GraphAlgo()
